5315b9c057b004e02cb09026cc9def3df65dc0d3
bernardo <bernardo.alemar@hotmail.com>
2024-06-17 13:14:32 -0300

    init e deleting sql

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import request as FlaskRequest
+from src.view.http_types.http_request import HttpRequest
+from src.view.http_types.http_response import HttpResponse
+
+def request_adapter(request: FlaskRequest) -> HttpResponse:
+    http_request = HttpRequest(
+        header=request.headers,
+        body=request.json,
+        query_params=request.args,
+        url=request.full_path,
+    )
+
+    return http_request
--- db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS 'users' (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    telefone TEXT NOT NULL,
-    email TEXT NOT NULL,
-     endereco TEXT NOT NULL
-);
