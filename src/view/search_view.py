9815eeccf23f6723c54dcb7b11cb2171dfa2bfe3
bernardo <bernardo.alemar@hotmail.com>
2024-06-27 15:16:43 -0300

    att search view with expect errors

--- src/view/search_view.py
+++ src/view/search_view.py
@@ -2,8 +2,8 @@ from .http_types.http_request import HttpRequest
 from .http_types.http_response import HttpResponse
 from src.view.interfaces.view_interface import ViewInterface
 from src.controller.interface.search_controller_interface import SearchControleInterface
-
-
+from src.errors.error_handler import handle_erros
+from src.errors.types.http_400 import Http400Error
 class SearchView(ViewInterface):
     def __init__(self, controller: SearchControleInterface) -> None:
         self.__controller = controller
@@ -17,4 +17,8 @@ class SearchView(ViewInterface):
 
             return HttpResponse(status_code=200, body={ "response": response })
         except Exception as exception:
-            return HttpResponse(status_code=500, body={ "error": str(exception) })
+            if str(exception) == "'NoneType' object has no attribute 'nome'":
+                return handle_erros(Http400Error('Usuário Não encontrado'))
+            elif str(exception) == "'nome'":
+                return handle_erros(Http400Error('Body incorreto, preencha dicionario com a key nome'))
+            return handle_erros(exception)
